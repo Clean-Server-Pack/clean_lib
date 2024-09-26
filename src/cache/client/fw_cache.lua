ae65c4c8d000117443b701c777cf77d09371155a
DirkDigglerz <liamsdargie@gmail.com>
2024-09-27 00:56:22 +0100

    test_fix?

--- src/cache/client/fw_cache.lua
+++ src/cache/client/fw_cache.lua
@@ -3,13 +3,13 @@ local settings = lib.settings
 if settings.framework == 'qb-core' then
   RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     local PlayerData =  lib.FW.Functions.GetPlayerData()
-    cache:set('playerLoaded', PlayerData)
     cache:set('citizenId', PlayerData.citizenid)
     cache:set('job', {
       name = PlayerData.job.name,
       grade = PlayerData.job.grade.level,
       onduty = PlayerData.job.onduty
     })
+    cache:set('playerLoaded', true)
   end)
 
   RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
@@ -24,6 +24,14 @@ if settings.framework == 'qb-core' then
     })
   end)
 
+  RegisterNetEvent('QBCore:Client:SetDuty', function(duty) 
+    cache:set('job', {
+      name = cache.job.name,
+      grade = cache.job.grade,
+      onduty = duty
+    })
+  end)
+
   RegisterNetEvent('QBCore:Client:UpdatePlayerData', function(data)
     cache:set('playerData', data)
   end)
@@ -33,13 +41,13 @@ elseif settings.framework == 'qbx_core' then
     if value then   
       local PlayerData =  lib.FW.Functions.GetPlayerData()
       if not PlayerData or not PlayerData.job then return end
-      cache:set('playerLoaded', true)
       cache:set('citizenId', PlayerData.citizenid)
       cache:set('job', {
         name = PlayerData.job.name,
         grade = PlayerData.job.grade.level,
         onduty = PlayerData.job.onduty
       })
+      cache:set('playerLoaded', true)
     else
       cache:set('playerLoaded', false)
       cache:set('job', {
@@ -50,24 +58,27 @@ elseif settings.framework == 'qbx_core' then
     end
   end)
 
-  RegisterNetEvent('QBCore:Client:OnJobUpdate', function(job)
-    local PlayerData = lib.FW.Functions.GetPlayerData()
+  RegisterNetEvent('QBCore:Client:SetDuty', function(duty) 
     cache:set('job', {
-      name = PlayerData.job.name,
-      grade = PlayerData.job.grade.level,
-      onduty = PlayerData.job.onduty
+      name = cache.job.name,
+      grade = cache.job.grade,
+      onduty = duty
     })
   end)
 
-  RegisterNetEvent('qbx_core:client:onGroupUpdate', function(groupName, groupGrade)
-    local PlayerData = lib.FW.Functions.GetPlayerData()
-    if groupName == 'job' then
-      cache:set('job', {
-        name = PlayerData.job.name,
-        grade = PlayerData.job.grade.level,
-        onduty = PlayerData.job.onduty
-      })
-    end
+  RegisterNetEvent('QBCore:Client:OnGangUpdate', function(gang) 
+    cache:set('gang', {
+      name  = gang.name,
+      grade = gang.rank,
+    })
+  end)
+
+  RegisterNetEvent('QBCore:Client:OnJobUpdate', function(job) 
+    cache:set('job', {
+      name = job.name,
+      grade = job.grade.level,
+      onduty = job.onduty
+    })
   end)
 elseif settings.framework == 'es_extended' then
   AddEventHandler('esx:setJob', function(job)
