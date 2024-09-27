1354a55b7566973638ec8ec60123ca0c09ecac19
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-27 21:15:22 +0000

    chore: bump manifest version to main

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ games { 'rdr3', 'gta5' }
 use_experimental_fxv2_oal 'yes'
 name         'clean_lib'
 author       'DirkScripts'
-version      '1.0.28'
+version      '1.0.29'
 description  'A library for FiveM developers to use in their projects, accepting of new features and contributions.'
 
 dependencies {
