a4cbdcd9201df43900cb80a42ff81eeaa6a8d86e
DirkDigglerz <liamsdargie@gmail.com>
2024-10-14 23:43:04 +0100

    new_rumair

--- web/src/utils/getImagePath.ts
+++ web/src/utils/getImagePath.ts
@@ -1,38 +1,52 @@
 import { getSettings } from "../providers/settings/settings_manager";
 
+async function checkImageExists(url: string) {
+  try {
+    const response = await fetch(url);
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+}
 
-
-export default function getImageType(image: string | undefined) {
-  if(!image) return false;    
+export default async function getImageType(image: string | undefined) {
+  if (!image) return false;
   const current_settings = getSettings();
   const is_link = image && typeof image === 'string' && (image.startsWith('https') || image.startsWith('nui://'));
-  // is_local_images are just plain something.png etc 
-  const is_inv_image = image && typeof image === 'string' && !image.startsWith('https');
+  const is_inv_image = image && typeof image === 'string' && !image.startsWith('https') && !image.startsWith('nui://');
   const is_icon = image && typeof image === 'string' && !image.startsWith('https') && !image.includes('.');
 
   if (is_link) {
     return {
       type: 'image',
       path: `${image}`,
-    }
+    };
   }
 
   if (is_icon) {
     return {
       type: 'icon',
       path: image,
-    }
+    };
   }
 
   if (is_inv_image) {
-    return {
-      type: 'image',
-      path: `${current_settings.itemImgPath}${image}`,
+    const extensions = ['.png', '.webp', '.jpg']; // Add more as needed
+    for (const ext of extensions) {
+      const fullPath = `${current_settings.itemImgPath}${image}${ext}`;
+      const exists = await checkImageExists(fullPath);
+      if (exists) {
+        return {
+          type: 'image',
+          path: fullPath,
+        };
+      }
     }
+    console.log(`[getImageType] Image not found: ${image}`);
   }
 
   return {
     type: 'unknown',
     path: '',
-  }
+  };
 }
