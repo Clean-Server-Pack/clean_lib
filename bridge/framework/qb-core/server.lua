1a563704c76ed8e404041d865f0db6664b359054
DirkDigglerz <liamsdargie@gmail.com>
2024-10-22 02:32:56 +0100

    qb-core stock useable item

--- bridge/framework/qb-core/server.lua
+++ bridge/framework/qb-core/server.lua
@@ -2,4 +2,8 @@ return {
   canUseItem = function(item)
     return lib.FW.Functions.CanUseItem(item)
   end,
+
+  useableItem = function(item, cb)
+    return lib.FW.Functions.CreateUseableItem(item,cb)
+  end,
 }
